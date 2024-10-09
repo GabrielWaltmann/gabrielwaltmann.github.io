8849cdeb3a10e2189cb41298338e25567d7f1f7f
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-10-09 16:51:33 -0300

    feat: add skill routes

---
+++ app/Http/Controllers/SkillsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Skill;
+
+class SkillsController extends Controller {
+    public function retrieves() {
+        $skills = Skill::all();
+        return response()->json($skills);
+    }
+
+    public function retrievesOne($id) {
+        $skill = Skill::find($id);
+        return response()->json($skill);
+    }
+
+    public function create() {
+        $skill = Skill::create(request()->all());
+        $skill-> save();
+
+        return response()->json($skill);
+    }
+
+    public function update($id) {
+        $skill = Skill::find($id);
+        $skill->update(request()->all());
+        return response()->json($skill);
+    }
+
+    public function delete($id) {
+        $skill = Skill::find($id);
+        $skill->delete();
+        return response()->json($skill);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SkillsController;
 
 Route::get('/api', [HelloController::class, 'index']);
+Route::get('/api/csrf-token', function () {
+    return csrf_token();
+});
+Route::post('/api/skills', [SkillsController::class, 'create']);
+Route::get('/api/skills', [SkillsController::class, 'retrieves']);
+Route::get('/api/skills/{id}', [SkillsController::class, 'retrievesOne']);
+Route::put('/api/skills/{id}', [SkillsController::class, 'update']);
+Route::delete('/api/skills/{id}', [SkillsController::class, 'delete']);
