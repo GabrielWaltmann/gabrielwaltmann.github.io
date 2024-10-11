13b5305704d9ebf65821be9fbdab1934c206c607
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-10-11 16:58:45 -0300

    feat: add header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    </head>
-    <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
 
-    <button class="btn">Hello daisyUI</button>
+        <title>Gabriel Waltmann</title>
+    </head>
+
+    <body>
+        <header class="w-full">
+            <ul class="flex flex-row gap-4 justify-end p-4">
+                <li><a class="hover:underline cursor-pointer">Home</a></li>
+                <li><a class="hover:underline cursor-pointer">About</a></li>
+                <li><a class="hover:underline cursor-pointer">Services</a></li>
+                <li><a class="hover:underline cursor-pointer">Experience</a></li>
+                <li><a class="hover:underline cursor-pointer">Works</a></li>
+                <li><a class="hover:underline cursor-pointer">Blog</a></li>
+                <li><a class="hover:underline cursor-pointer">Contact</a></li>
+            </ul>
+        </header>
+
+        {{-- <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+
+        <button class="btn">Hello daisyUI</button> --}}
     </body>
 </html>
