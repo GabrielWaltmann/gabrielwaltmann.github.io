b25df7c25bbce1af06757839381af4d903d8189c
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-10-17 15:34:21 -0300

    style: fix mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,64 +61,75 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="description" content="{{ $profile_description }}>
+        <meta name="description" content="{{ $profile_description }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
 
-        {{-- resources --}}
+        {{-- Phosphor icons --}}
         <script src="https://unpkg.com/phosphor-icons"></script>
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
         <title>{{ $profile_name }}</title>
     </head>
 
-    <body>
-        <header class="w-full">
-            <ul class="flex flex-row gap-4 justify-end p-4">
-                <li><a class="hover:underline cursor-pointer">Home</a></li>
-                <li><a class="hover:underline cursor-pointer">About</a></li>
-                <li><a class="hover:underline cursor-pointer">Services</a></li>
-                <li><a class="hover:underline cursor-pointer">Experience</a></li>
-                <li><a class="hover:underline cursor-pointer">Works</a></li>
-                <li><a class="hover:underline cursor-pointer">Blog</a></li>
-                <li><a class="hover:underline cursor-pointer">Contact</a></li>
-            </ul>
+    <body class="bg-gray-50 h-screen w-screen overflow-x-hidden">
+        {{-- Header --}}
+        <header class="flex flex-row justify-end p-2 w-full">
+            {{-- Toggle language --}}
+            <nav>
+                <button class="rounded-xl" id="language-toggle" onclick="toggleLanguage()">
+                    {{-- <span>🇧🇷</span> --}}
+                    <span>🇺🇸</span>
+                </button>
+            </nav>
         </header>
 
-        <main>
-            <section class="w-full flex flex-col justify-center h-[50vh] items-center gap-4">
+        <main class="flex px-2 flex-col gap-4 w-full">
+            <section class="w-full flex flex-row justify-between items-center gap-2">
                 {{-- Profile Image --}}
-                <picture class="w-[160px]">
+                <picture>
                     <img
                         class="rounded-full"
                         src="profile.jpg"
                         height="100%"
-                        width="100%"
+                        width="100rem"
                         alt="Profile picture"
                     >
                 </picture>
 
                 {{-- Profile Details --}}
-                <article class="text-center">
-                    <h1 class="text-3xl font-bold">{{ $profile_name }}</h1>
-                    <h2 class="text-xl">{{ $profile_title }}</h2>
+                <article class="flex-1 flex flex-col">
+                    <h1 class="text-2xl font-bold">{{ $profile_name }}</h1>
+                    <h2 class="text-lg">{{ $profile_title }}</h2>
+
+                    {{-- Social Media Links --}}
+                    <ul class="flex flex-row gap-4 mt-4">
+                        <li>
+                            <a target="_blank" href="{{ $social_github }}"><i class="ph ph-github-logo text-2xl"></i></a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="{{ $social_linkedin }}"><i class="ph ph-linkedin-logo text-2xl"></i></a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="{{ $social_instagram }}"><i class="ph ph-instagram-logo text-2xl"></i></a>
+                        </li>
+
+                        <li>
+                            <a target="_blank" href="{{ $social_email }}"><i class="ph ph-envelope-simple text-2xl"></i></a>
+                        </li>
+                    </ul>
                 </article>
+            </section>
 
-                {{-- Social Media Links --}}
-                <ul class="flex flex-row gap-4">
-                    <li>
-                        <a target="_blank" href="{{ $social_github }}"><i class="ph ph-github-logo text-3xl"></i></a>
-                    </li>
-                    <li>
-                        <a target="_blank" href="{{ $social_linkedin }}"><i class="ph ph-linkedin-logo text-3xl"></i></a>
-                    </li>
-                    <li>
-                        <a target="_blank" href="{{ $social_instagram }}"><i class="ph ph-instagram-logo text-3xl"></i></a>
-                    </li>
+            <section class="w-full flex flex-col justify-center items-center gap-2">
+                <h1 class="text-2xl font-bold text-start w-full">About Me</h1>
+                <p class="text-base text-justify text-wrap">{{ $profile_description }}</p>
+            </section>
 
-                    <li>
-                        <a target="_blank" href="{{ $social_email }}"><i class="ph ph-envelope-simple text-3xl"></i></a>
-                    </li>
-                </ul>
+            <section class="w-full flex flex-col justify-center bg-gray-50 items-center gap-2 overflow-x-hidden">
+                <h1 class="text-2xl font-bold text-start w-full">Projects</h1>
+                <p class="text-base text-start w-full">A collection of projects I've worked on.</p>
             </section>
         </main>
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,25 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            primary: '#6BBAEC',
+        },
+    },
+    fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '3.815rem',
+        '7xl': '4.768rem',
+        '8xl': '6.133rem',
+        '9xl': '8.267rem',
+        '10xl': '10.67rem',
+    },
   },
-  plugins: [
-    require('daisyui'),
-  ],
 }
 
